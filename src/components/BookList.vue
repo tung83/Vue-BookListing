a1bc075f6c764b0d23b2bbe8efbac03cf63b3735
Tung Nguyen <tungng@augensoftwaregroup.com>
2018-11-02 14:34:01 +0700

    Completed the module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1> {{ title}}</h1>
+        <ul>
+            <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+        </ul>
     </div>
 </template>
 <script>
@@ -8,7 +11,13 @@ export default {
     name: 'BookList',
     data(){
         return {
-            title: 'All Books'
+            title: 'All Books',
+            books: [
+                {title: 'Tung', author: 'Tung test'},
+                {title: 'Tung2', author: 'Tung test2'},
+                {title: 'Tung3', author: 'Tung test3'},
+            ]
+
         }
     }
 }
@@ -17,5 +26,13 @@ export default {
 <style>
 h1, h2 {
    font-weight: normal 
+}
+ul{
+    list-style-type: none;
+    padding: 0;
+}
+li{
+    display: block;
+    margin: 0 10px;
 }
 </style>
